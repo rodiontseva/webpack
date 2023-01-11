d12defe609747e7c1701429a5651113f8d7c4761
Olena Rodiontseva <elena.rodiontseva@gmail.com>
2023-01-11 22:07:35 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./style/main.scss";
+
+
+ReactDOM.render(
+    <div className='main'>Wabpack is working!</div>,
+  document.getElementById('root')
+);
