dbeecc7f2d7700559a8d23c68fb1bf979aeb3293
Olena Rodiontseva <elena.rodiontseva@gmail.com>
2023-01-24 22:56:58 +0200

    save

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -60,6 +61,7 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(
       { filename: '[name].[contenthash].bundle.css', ignoreOrder: true }
     ),
